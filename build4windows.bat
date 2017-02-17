e221082f0a423edfa89739e2289dba3d2ec696d4
Juergen Kosel <juergen.kosel@gmx.de>
2017-02-17 21:25:49 +0100

    Corrected build4windows script to build installer
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- build4windows.bat
+++ build4windows.bat
@@ -11,11 +11,11 @@ REM Ensure that cygwin bash is found before Windows10 Ubunutu bash:
 set PATH=%WIRESHARK_CYGWIN_INSTALL_PATH%\bin;%PATH%
 
 cd wireshark-cmake
-"C:\Program Files\CMake\bin\cmake" -DENABLE_CHM_GUIDES=off ..\wireshark
+"C:\Program Files\CMake\bin\cmake" -DENABLE_CHM_GUIDES=on ..\wireshark
 pause
 Rem build wireshark
 msbuild /m /p:Configuration=RelWithDebInfo Wireshark.sln
 pause
 Rem build the installer
-REM msbuild /m /p:Configuration=RelWithDebInfo nsis_package_prep.vcxproj
-REM msbuild /m /p:Configuration=RelWithDebInfo nsis_package.vcxproj
+msbuild /m /p:Configuration=RelWithDebInfo nsis_package_prep.vcxproj
+msbuild /m /p:Configuration=RelWithDebInfo nsis_package.vcxproj
