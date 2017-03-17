e180ed8e96eefc78e7db082e7b29b7ea70e04f18
Juergen Kosel <juergen.kosel@gmx.de>
2017-03-17 19:08:25 +0100

    Get the location of the sources from the call of build4windows.bat
    Determine the variables
    	repo-root - as the position of the repository and sources
    	WIRESHARK_BASE_DIR - as the directory above the sources
    from the dirname of the called build4windows.bat script.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- build4windows.bat
+++ build4windows.bat
@@ -1,8 +1,14 @@
 REM Build preparation script for wireshark for windows with Visual Studio 2015
 REM See https://www.wireshark.org/docs/wsdg_html_chunked/ChSetupWin32.html
 REM Modify this script to your needs and run it from the MSVC command prompt
+
+set repo-root=%~dp0
+rem // remove trailing slash
+set repo-root=%repo-root:~0,-1%
+echo Building from source in %repo-root% ...
+
 set CYGWIN=nodosfilewarning
-set WIRESHARK_BASE_DIR=C:\Programmierung
+set WIRESHARK_BASE_DIR=%repo-root%\..
 set WIRESHARK_TARGET_PLATFORM=win32
 set QT5_BASE_DIR=C:\Qt\5.6\msvc2015
 set WIRESHARK_CYGWIN_INSTALL_PATH=C:\cygwin64
@@ -11,7 +17,7 @@ REM Ensure that cygwin bash is found before Windows10 Ubunutu bash:
 set PATH=%WIRESHARK_CYGWIN_INSTALL_PATH%\bin;%PATH%
 
 cd wireshark-cmake
-"C:\Program Files\CMake\bin\cmake" -DENABLE_CHM_GUIDES=on ..\wireshark
+"C:\Program Files\CMake\bin\cmake" -DENABLE_CHM_GUIDES=on %repo-root%
 pause
 Rem build wireshark
 msbuild /m /p:Configuration=RelWithDebInfo Wireshark.sln
