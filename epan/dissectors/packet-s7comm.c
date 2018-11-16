e5872655f4711a02bc6dcfa1833ad0ef23c1012b
Juergen Kosel <juergen.kosel@gmx.de>
2018-11-16 18:13:46 +0100

    Add additional syntax ids 0x83 and 0x84 for NCK data access
    Beside the standard NCK syntax id 0x82 the ids 0x83 and 0x84 are used
    for values with metric and inch units.
    
    Change-Id: I62bf2d2e583905c9fa90e4e7caa614a6fe6a7155
    Signed-off-by: Juergen Kosel <juergen.kosel@gmx.de>

--- epan/dissectors/packet-s7comm.c
+++ epan/dissectors/packet-s7comm.c
@@ -439,7 +439,9 @@ static const value_string item_transportsizenames[] = {
 #define S7COMM_SYNTAXID_DRIVEESANY          0xa2        /* seen on Drive ES Starter with routing over S7 */
 #define S7COMM_SYNTAXID_1200SYM             0xb2        /* Symbolic address mode of S7-1200 */
 #define S7COMM_SYNTAXID_DBREAD              0xb0        /* Kind of DB block read, seen only at an S7-400 */
-#define S7COMM_SYNTAXID_NCK                 0x82        /* Sinumerik NCK HMI access */
+#define S7COMM_SYNTAXID_NCK                 0x82        /* Sinumerik NCK HMI access (current units) */
+#define S7COMM_SYNTAXID_NCK_METRIC          0x83        /* Sinumerik NCK HMI access metric units */
+#define S7COMM_SYNTAXID_NCK_INCH            0x84        /* Sinumerik NCK HMI access inch */
 
 static const value_string item_syntaxid_names[] = {
     { S7COMM_SYNTAXID_S7ANY,                "S7ANY" },
@@ -453,6 +455,8 @@ static const value_string item_syntaxid_names[] = {
     { S7COMM_SYNTAXID_1200SYM,              "1200SYM" },
     { S7COMM_SYNTAXID_DBREAD,               "DBREAD" },
     { S7COMM_SYNTAXID_NCK,                  "NCK" },
+    { S7COMM_SYNTAXID_NCK_METRIC,           "NCK_M" },
+    { S7COMM_SYNTAXID_NCK_INCH,             "NCK_I" },
     { 0,                                    NULL }
 };
 
@@ -2556,7 +2560,7 @@ s7comm_syntaxid_1200sym(tvbuff_t *tvb,
 /*******************************************************************************************************
  *
  * Addressdefinition for Sinumeric NCK access
- * type == 0x12, length == 8, syntax-ID == 0x82
+ * type == 0x12, length == 8, syntax-ID == 0x82 or == 0x83 or == 0x84
  *
  *******************************************************************************************************/
 static guint32
@@ -2661,7 +2665,10 @@ s7comm_decode_param_item(tvbuff_t *tvb,
     } else if (var_spec_type == 0x12 && var_spec_length >= 14 && var_spec_syntax_id == S7COMM_SYNTAXID_1200SYM) {
         /* TIA S7 1200 symbolic address mode */
         offset = s7comm_syntaxid_1200sym(tvb, offset, item_tree, var_spec_length);
-    } else if (var_spec_type == 0x12 && var_spec_length == 8 && var_spec_syntax_id == S7COMM_SYNTAXID_NCK) {
+    } else if (var_spec_type == 0x12 && var_spec_length == 8
+               && ((var_spec_syntax_id == S7COMM_SYNTAXID_NCK)
+                   || (var_spec_syntax_id == S7COMM_SYNTAXID_NCK_METRIC)
+                   || (var_spec_syntax_id == S7COMM_SYNTAXID_NCK_INCH))) {
         /* Sinumerik NCK access */
         offset = s7comm_syntaxid_nck(tvb, offset, item_tree);
     } else if (var_spec_type == 0x12 && var_spec_length == 10 && var_spec_syntax_id == S7COMM_SYNTAXID_DRIVEESANY) {
